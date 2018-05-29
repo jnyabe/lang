2ef15a65756bdf5a98e2759e570d5311d96bbe45
Jun Yabe <yabe@helvellyn.local>
2018-05-30 01:13:48 +0900

    Update sample

--- java/multi_thread/00.Introduction/Main.java
+++ java/multi_thread/00.Introduction/Main.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
 public class Main {
 	public static void main(String[] args)
 	{
@@ -12,8 +15,25 @@ public class Main {
 		new PrintThread("Good!").start();
 		new PrintThread("Nice!").start();
 		*/
-		new Thread(new Printer("Good!")).start();
-		new Thread(new Printer("Nice!")).start();
-		
+		/*
+		  new Thread(new Printer("Good!")).start();
+		  new Thread(new Printer("Nice!")).start();
+		*/
+		/* I1-8
+		   ThreadFactory factory = Executors.defaultThreadFactory();
+		   factory.newThread(new Printer("Nice!")).start();
+		   for(int i=0; i < 1000; i++){
+		   System.out.print("Good!");
+		}
+		*/
+		/* I1-9 */
+		for(int i=0; i < 10; i++) {
+			System.out.print("Good!");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				
+			}
+		}
 	}
 }
